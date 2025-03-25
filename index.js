eab54a892d911c914d166bda687e133c6387a344
Malik-gee <balogunmaleekbolaji@gmail.com>
2025-03-25 07:07:57 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+// // Get the elements using getElementById
+// const balance = document.getElementById("balance");
+// const income = document.getElementById("income");
+// const outcome = document.getElementById("outcome");
+// const transactionList = document.getElementById("transaction-list");
+//  const submitTransaction = document.getElementById("add-Transaction");
+// // const description = document.getElementById("description");
+// // const amount = document.getElementById("amount");
+
+
+
+
+// document.getElementById("add-transaction").onclick = function(){
+//     description = document.getElementById("description").value
+//     amount = document.getElementById("amount").value
+//      document.querySelector(`input[name="type"]:checked`).value
+//     console.log(description, amount, type)
+// }
+
+document.getElementById("add-transaction").addEventListener('click',()=>{
+      const description = document.getElementById('description').value;
+   console.log(description,)
+        
+    
+    
+ })
+
+// console.log(description,)
+
+
+
+// }
+
+// document.querySelectorAll('.description', '.amount').forEach(item =>{
+//     element.addEventListener('clikc',()=>{
+//         console.log(element.value)
+        
+//     })
+// })
+
+// document.getElementById("add-transaction").addEventListener('click', () => {
+//     const description = document.getElementById("description").value;
+//     const amount = document.getElementById("amount").value;
+//     const type = document.querySelector('input[name="type"]:checked').value;
+//     console.log(description, amount);
+//     console.log(description, amount, type);
+// });
